eaa7e50c948c756d607732710e0b4bf03d037db1
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-11-18 09:36:58 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player1 = new Player();
+        player1.playerName = "First Player";
+        player1.level = 1;
+        player1.health = 100;
+
+        Player player2 = new Player("Second Player", 50, 1000);
+
+        Console.WriteLine("\nPlayer 1");
+        Console.WriteLine($"Name   : {player1.playerName}");
+        Console.WriteLine($"Level  : {player1.level}");
+        Console.WriteLine($"Health : {player1.health}");
+
+        Console.WriteLine("\nPlayer 2");
+        Console.WriteLine($"Name   : {player2.playerName}");
+        Console.WriteLine($"Level  : {player2.level}");
+        Console.WriteLine($"Health : {player2.health}");
+
+
+    }
+}
